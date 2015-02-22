1848bbaa0c033e9b41768ab9f6908e4a6347d382
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2015-02-21 20:38:52 -0500

    Layout basics for command and test it is working as expected.

--- materialscommons.go
+++ materialscommons.go
@@ -4,62 +4,88 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/samalba/dockerclient"
+	"github.com/fsouza/go-dockerclient"
+	"github.com/jessevdk/go-flags"
 )
 
-type serverOptions struct {
+type StartCommand struct {
+	Name string `long:"name" description:"Materials Commons instance name"`
+}
+
+type RestartCommand struct {
+	Name string `long:"name" description:"Materials Commons instance name"`
+}
+
+type StopCommand struct {
+	Name string `long:"name" description:"Materials Commons instance name"`
+}
+
+type DeployCommand struct {
 	MCDir string `long:"mcdir" description:"Directory path to materials commons file storage"`
 	Name  string `long:"name" description:"Materials Commons instance name"`
 }
 
-type startCommand struct {
-}
-
-type restartCommand struct {
-}
-
-type stopCommand struct {
-}
-
-type deployCommand struct {
-}
-
-type upgradeCommand struct {
-}
-
-type Options struct {
-	Server serverOptions `group:"Server Options"`
+type UpgradeCommand struct {
 }
 
 var (
-	docker dockerclient.Client
+	client         docker.Client
+	startCommand   StartCommand
+	restartCommand RestartCommand
+	stopCommand    StopCommand
+	deployCommand  DeployCommand
+	upgradeCommand UpgradeCommand
 )
 
 func main() {
-	docker, err := dockerclient.NewDockerClient("", nil)
+	parser := flags.NewParser(nil, flags.Default)
+	_, err := parser.Parse()
+	parser.AddCommand("start", "Start a MaterialsCommons instance", "", &startCommand)
+	parser.AddCommand("restart", "Restarts a MaterialsCommons instance", "", &restartCommand)
+	parser.AddCommand("stop", "Stops a MaterialsCommons instance", "", &stopCommand)
+	parser.AddCommand("deploy", "Deploys a new MaterialsCommons instance", "", &deployCommand)
+	parser.AddCommand("upgrade", "Upgrades a MaterialsCommons instance", "", &upgradeCommand)
+
 	if err != nil {
-		fmt.Printf("Unable to connect to docker: %s", err)
+		fmt.Printf("Failed parsing commands: %s\n", err)
 		os.Exit(1)
 	}
-	var _ = docker
+
+	endpoint := "unix:///var/run/docker.sock"
+	client, err := docker.NewClient(endpoint)
+	if err != nil {
+		fmt.Printf("Failed to connect to docker: %s\n", err)
+		os.Exit(1)
+	}
+
+	var _ = client
+
+	fmt.Println("Connected to docker!")
+
+	// docker, err := dockerclient.NewDockerClient("", nil)
+	// if err != nil {
+	// 	fmt.Printf("Unable to connect to docker: %s", err)
+	// 	os.Exit(1)
+	// }
+	// var _ = docker
 }
 
-func (cmd *startCommand) Execute(args []string) error {
+func (cmd *StartCommand) Execute(args []string) error {
 	return nil
 }
 
-func (cmd *restartCommand) Execute(args []string) error {
+func (cmd *RestartCommand) Execute(args []string) error {
 	return nil
 }
 
-func (cmd *stopCommand) Execute(args []string) error {
+func (cmd *StopCommand) Execute(args []string) error {
 	return nil
 }
 
-func (cmd *deployCommand) Execute(args []string) error {
+func (cmd *DeployCommand) Execute(args []string) error {
 	return nil
 }
 
-func (cmd *upgradeCommand) Execute(args []string) error {
+func (cmd *UpgradeCommand) Execute(args []string) error {
 	return nil
 }
